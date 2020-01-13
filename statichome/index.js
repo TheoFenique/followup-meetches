580b5b6498bd28f2403c1ec832c53e2fffd7fda1
Theo Fenique <theo.fenique@gmail.com>
2020-01-13 22:50:15 +0100

    :sparkles: Improved details, still need the js sliders form the landing and blog

---
+++ statichome/index.js
@@ -0,0 +1,25 @@
+const hiwImages = [...document.querySelectorAll(".howitworks-container .slider img")]
+const hiwButton = [...document.querySelectorAll("button.principle")]
+const hiwText = [...document.querySelectorAll("p.principle")]
+const hiwContent = {
+    hiwImages,
+    hiwButton,
+    hiwText
+}
+
+
+console.log(hiwContent)
+
+let hiwSlider = (elements) => {
+    elements.hiwButton.map((v, i, a) => {
+        v.addEventListener("click", (_e) => {
+            elements.hiwButton.map((v, i, a) => { v.classList.remove("active") })
+            elements.hiwText.map((v, i, a) => { v.classList.remove("active") })
+            elements.hiwImages.map((v, i, a) => { v.classList.remove("active") })
+            v.classList.add("active")
+            hiwText[i].classList.add("active")
+            hiwImages[i].classList.add("active")
+        })
+    })
+}
+hiwSlider(hiwContent)
